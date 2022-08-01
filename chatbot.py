76fbfc461edcb55384d5dee29dd87e03a03af45e
Francisco Alejandro Villalba Ferrer <franciscovillalbaferrer7@gmail.com>
2022-08-01 09:44:12 -0400

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,6 @@
 import re
 import random
+import datetime
 
 #recibe la entrada del usuario y se remueven los signos de puntuación con expresiones regulares
 #receives user input and removes punctuation with regular expressions
@@ -29,6 +30,28 @@ def message_probability(user_message, recognized_words, single_response=False, r
     else:
         return 0
 
+def localtime():
+    now = datetime.datetime.now()
+    timenow = int(datetime.datetime.strftime(now, '%H'))
+    message_hour = ''
+    if timenow < 5:
+        message_hour = 'Buenas noches'
+    elif timenow < 12:
+        message_hour = 'Buenos días'
+    elif timenow < 19:
+        message_hour = 'Buenas tardes'
+    else:
+        message_hour = 'Buenas noches'
+    return message_hour
+
+#Listas de preguntas y respuestas
+greetings = [('Hola, ' + localtime() + ', me llamo Tobias y soy una creacion de Francisco Villalba'), ('Hola, es un gusto en presentarme. Me llamo Tobias y soy un chatbot creado por Francisco Villalba, usando operaciones recurrentes')]
+greetingsr = ['hola', 'saludos', 'buenas', 'buenos', 'buen', 'días', 'día', 'tardes', 'noches']
+greetings1 = ['Por ahora estoy bien, me siento muy contento por ir recibiendo nuevas actualizaciones ¿y tú?', 'Estoy bien y usted ¿cómo se encuentra?']
+greetingsr1 = ['como', 'estas', 'cómo', 'sientes', 'vas']
+thanks = ['Siempre a la orden para servirte', 'Me alegra mucho haberte podido ayudar', 'Me hace feliz poder ayudarte']
+thanksr1 = ['gracias', 'agradecido', 'thanks', 'agradecida', 'te lo agradezco']
+
 def check_all_messages(message):
         highest_prob = {}
 
@@ -36,10 +59,9 @@ def check_all_messages(message):
             nonlocal highest_prob
             highest_prob[bot_response] = message_probability(message, list_of_words, single_response, required_words)
 
-        response('Hola', ['hola', 'klk', 'saludos', 'buenas'], single_response = True)
-        response('Estoy bien y tu?', ['como', 'estas', 'va', 'vas', 'sientes'], required_words=['como'])
-        response('Estamos ubicados en la calle 23 numero 123', ['ubicados', 'direccion', 'donde', 'ubicacion'], single_response=True)
-        response('Siempre a la orden', ['gracias', 'te lo agradezco', 'thanks'], single_response=True)
+        response(greetings[random.randrange(len(greetings))], greetingsr, single_response = True)
+        response(greetings1[random.randrange(len(greetings1))], greetingsr1, single_response= True)
+        response(thanks[random.randrange(len(thanks))], thanksr1, single_response=True)
 
         best_match = max(highest_prob, key=highest_prob.get)
         #print(highest_prob) #elimina el primer hashtag para probar el algoritmo, remove the first hashtag to test the algorithm
@@ -47,7 +69,7 @@ def check_all_messages(message):
         return unknown() if highest_prob[best_match] < 1 else best_match
 
 def unknown():
-    response = ['puedes decirlo de nuevo?', 'No estoy seguro de lo quieres', 'búscalo en google a ver que tal'][random.randrange(3)]
+    response = ['puedes decirlo de nuevo?', 'No estoy seguro de lo quieres', 'puedes probar a buscarlo en google a ver que tal', 'puedes ayudarme un poco, no te estoy entiendiendo'][random.randrange(4)]
     return response
 
 #bucle para pedir siempre las preguntas al usuario
